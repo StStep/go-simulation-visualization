bd622108e47334b9bba93dea9bc33692e9f6350c
R. Stephens Summerlin <st.summerlin@gmail.com>
2018-09-30 21:40:34 -0400

    Added example logfmt file

--- simvis.py
+++ simvis.py
@@ -6,13 +6,7 @@ Visualize a simulation log file
 import numpy as np
 from matplotlib import pyplot as plt
 from matplotlib import animation
-
-fig = plt.figure()
-fig.set_dpi(100)
-fig.set_size_inches(7, 6.5)
-
-ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
-patch = plt.Circle((0, 0), 0.2, fc='y')
+from logfmt import parse
 
 def init():
     patch.center = (0, 0)
@@ -26,11 +20,23 @@ def animate(i):
     patch.center = (x, y)
     return patch,
 
-anim = animation.FuncAnimation(fig, animate, 
-                               init_func=init, 
-                               frames=360, 
-                               interval=20,
-                               blit=True)
+with open('test/example.logfmt') as f:
+    for value in parse(f):
+        print(value)
 
-plt.show()
-anim.save('animation.gif');
+if False:
+    fig = plt.figure()
+    fig.set_dpi(100)
+    fig.set_size_inches(7, 6.5)
+    
+    ax = plt.axes(xlim=(-10, 10), ylim=(-10, 10))
+    patch = plt.Circle((0, 0), 0.2, fc='y')
+
+    anim = animation.FuncAnimation(fig, animate, 
+                                   init_func=init, 
+                                   frames=360, 
+                                   interval=20,
+                                   blit=True)
+    
+    plt.show()
+    anim.save('animation.gif');
